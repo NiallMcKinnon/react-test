12273b4b02d8d87819fc23a8e32969fbfadea679
Niall McKinnon <nrm3009@uncw.edu>
2023-03-25 13:57:10 -0400

    Added multiple squares

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React from "react";
+// Returns a component:
 export default function Square() {
-  return <button className="square">X</button>;
+    // return <button className="square">X</button>;
+    return (
+        <React.Fragment>
+            <button className="square">X</button>
+            <button className="square">X</button>
+            <button className="square">X</button>
+        </React.Fragment>
+    );
 }
