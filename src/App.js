7138be9c35072a99bdeb748c0fbf0cc7f21e9db9
Niall McKinnon <nrm3009@uncw.edu>
2023-03-25 13:58:04 -0400

    Added full board

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ export default function Square() {
     // return <button className="square">X</button>;
     return (
         <React.Fragment>
-            <button className="square">X</button>
-            <button className="square">X</button>
-            <button className="square">X</button>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
         </React.Fragment>
     );
 }
